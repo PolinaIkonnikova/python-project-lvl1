0278ce0f9358a9b675758398fa26e7ae61db70a9
Polina Ikonnikova <spollyv@gmail.com>
2021-12-12 15:21:04 +0500

    fixitg E226 in progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,9 +5,9 @@ def progression():
     differ = random.randint(2, 6)
     length = random.randint(8, 12)
     start = random.randint(1, 20)
-    end = (start + differ*length)
+    end = (start + differ * length)
     progress_list = [str(x) for x in range(start, end, differ)]
-    replacement = random.randint(0, (length-1))
+    replacement = random.randint(0, (length - 1))
     repl_number = progress_list[replacement]
     progress_list[replacement] = '..'
     issue = ''
