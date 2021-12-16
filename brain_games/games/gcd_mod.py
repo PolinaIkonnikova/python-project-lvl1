60a9032fd3e044d4906e0fecbe817b817394bced
Polina Ikonnikova <spollyv@gmail.com>
2021-12-16 23:50:57 +0500

    changed gcd_mod

--- brain_games/games/gcd_mod.py
+++ brain_games/games/gcd_mod.py
@@ -13,10 +13,9 @@ def func_game():
 
 
 def gcd(num_1, num_2):
-    if num_1 != num_2:
-        while num_1 != num_2:
-            if num_1 > num_2:
-                num_1 = num_1 - num_2
-            else:
-                num_2 = num_2 - num_1
-    return num_1
+    while num_1 != 0 and num_2 != 0:
+        if num_1 > num_2:
+            num_1 = num_1 % num_2
+        else:
+            num_2 = num_2 % num_1
+    return num_1 + num_2
